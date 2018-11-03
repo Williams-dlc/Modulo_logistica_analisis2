cc7aed33f8f60259d98d743d1478a539703c13a1
ValeryAS2 <41198395+ValeryAS2@users.noreply.github.com>
2018-11-03 00:11:19 -0600

    Add files via upload

---
+++ PolizasLogistica/MantenimientosMarca/MantenimientosLinea/MarcaLogistica.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MantenimientosLinea;
+using CapaDiseno;
+
+namespace MantenimientosLinea
+{
+    public partial class MarcaLogistica : Form
+    {
+        Navegador nv = new Navegador();
+        public MarcaLogistica()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void Btn_cerra_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LineaLogistica_Load(object sender, EventArgs e)
+        {
+            DataTable tabla = nv.cargarDatos("TBL_Marca");
+            dataGridView1.DataSource = tabla;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            new InterfazMantenimientoMarca(dataGridView1).Show();
+        }
+    }
+}
