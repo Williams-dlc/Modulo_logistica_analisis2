3bec0bfc77455a1e19d6c0434dfdb39421353897
Williams-dlc <41239301+Williams-dlc@users.noreply.github.com>
2018-11-03 01:07:57 -0600

    Add files via upload

---
+++ TrasladoProductos/TrasladoProductos/TrasladoProductos/CapaDatos_TrasladoProductos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data;
+
+namespace TrasladoProductos
+{
+    class CapaDatos_TrasladoProductos
+    {
+        static OdbcConnection cnx = null;
+
+        public static OdbcConnection getDB()
+        {
+            cnx = new OdbcConnection(Properties.Settings.Default.ruta);
+            cnx.Open();
+            return cnx;
+        }
+    }
+}
---
+++ TrasladoProductos/TrasladoProductos/TrasladoProductos/CapaLogica_TrasladoProducto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using System.Data;
+using System.Windows.Forms;
+namespace TrasladoProductos
+{
+    class CapaLogica_TrasladoProducto
+    {
+        OdbcConnection conectar = CapaDatos_TrasladoProductos.getDB();
+        public DataSet plusDatos(int id,string tabla, string campo)
+        {
+            DataSet set = new DataSet();
+            try
+            {
+                using (var cmd = conectar.CreateCommand())
+                {
+                    cmd.CommandText = "Select" + campo + "from" + tabla + "where" +id+"=" + id;
+                    OdbcDataAdapter datos = new OdbcDataAdapter(cmd);
+                    set = new DataSet();
+                    datos.Fill(set);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return set;
+        }
+    }
+}
